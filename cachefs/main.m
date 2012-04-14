31fcf61773c38f207fa017b05b0fe125ef331527
Matias Eyzaguirre <dented42@me.com>
2012-04-14 16:27:37 -0600

    Initial Commit

---
+++ cachefs/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cachefs
+//
+//  Created by Matias Eyzaguirre on 4/14/12.
+//  Copyright (c) 2012 J. Willard Marriott Library. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
